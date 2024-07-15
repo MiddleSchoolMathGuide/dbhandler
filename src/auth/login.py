14b77c7bb097ce4e4a926c791b0ffdd5477c3e1f
Wrench56 <dmarkreg@gmail.com>
2024-07-15 03:30:57 -0400

    Change return signature of `login.py`

--- src/auth/login.py
+++ src/auth/login.py
@@ -6,14 +6,14 @@ from .. import ghandler
 from . import session
 
 
-def login(username: str, password_hash: str) -> dict:
+def login(username: str, password_hash: str) -> tuple[dict, str | None]:
     '''Validate user'''
     user = ghandler.db['users'].find_one({'username': username})
     if not user:
-        return {'ok': False, 'message': 'User not found'}
+        return {'ok': False, 'message': 'User not found'}, None
 
     if password_hash != user.get('password_hash'):
-        return {'ok': False, 'message': 'Invalid password'}
+        return {'ok': False, 'message': 'Invalid password'}, None
 
     id_ = session.create(user['_id'])
-    return {'ok': True, 'message': 'Authentication successful', 'id': id_}
+    return {'ok': True, 'message': 'Authentication successful'}, id_
