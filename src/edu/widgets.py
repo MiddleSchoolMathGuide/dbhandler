640ae9ff7884661399703be888ae80fc4f6520a5
Wrench56 <dmarkreg@gmail.com>
2024-07-18 06:16:54 -0400

    Add `widgets.py`

---
+++ src/edu/widgets.py
@@ -0,0 +1,31 @@
+'''
+Manages units
+'''
+
+from bson import ObjectId
+from .. import ghandler
+
+
+def set(id: str, widget_data: dict[str, any]) -> str:
+    '''
+    Set fields of a widget
+    '''
+
+    widget_id = (
+        ghandler.db['widgets']
+        .update_one(
+            {'_id': widget_data.get('_id') or ObjectId()},
+            {'$set': {'lesson': id}.update(widget_data)},
+            upsert=True
+        )
+        .upserted_id
+    )
+
+    return widget_id
+
+
+def get_units(id: str) -> tuple[dict[str, any], ...]:
+    '''
+    Return all the widgets connected to a lesson
+    '''
+    return tuple(ghandler.db['widgets'].find({'lesson_id': id}))
