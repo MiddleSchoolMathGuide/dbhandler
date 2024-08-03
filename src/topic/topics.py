ce1c54a1f880005c99e32437c28cb8d81fe3271e
Wrench56 <dmarkreg@gmail.com>
2024-08-03 05:24:16 -0400

    Fix `topics.py` formatting

--- src/topic/topics.py
+++ src/topic/topics.py
@@ -1,6 +1,6 @@
-"""
+'''
 Manages topics
-"""
+'''
 
 from typing import Any
 
@@ -15,15 +15,15 @@ from . import widgets
 
 
 def set(topic_data: dict[str, any]) -> str:
-    """
+    '''
     Set fields of a topic
-    """
+    '''
 
-    units_ = topic_data.pop("units", [])
-    topic_id = ObjectId(topic_data.pop("_id", None))
+    units_ = topic_data.pop('units', [])
+    topic_id = ObjectId(topic_data.pop('_id', None))
 
-    ghandler.db["topics"].update_one(
-        {"_id": topic_id}, {"$set": topic_data}, upsert=True
+    ghandler.db['topics'].update_one(
+        {'_id': topic_id}, {'$set': topic_data}, upsert=True
     )
 
     unit_ids = []
@@ -35,76 +35,76 @@ def set(topic_data: dict[str, any]) -> str:
 
 
 def delete(title: str) -> None:
-    """
+    '''
     Delete a topic
-    """
-    ghandler.db["topics"].delete_one({"title": utils.normalize_title(title)})
+    '''
+    ghandler.db['topics'].delete_one({'title': utils.normalize_title(title)})
 
 
 def set_unit_ids(id: ObjectId, unit_ids: list[str]) -> None:
-    """
+    '''
     Set unit ids of a topic
-    """
-    ghandler.db["topics"].update_one({"_id": id}, {"$set": {"units": unit_ids}})
+    '''
+    ghandler.db['topics'].update_one({'_id': id}, {'$set': {'units': unit_ids}})
 
 
 def get_units(title: str) -> None:
-    pipeline = [{"$match": {"tilte": utils.normalize_title(title)}}]
-    return list(ghandler.db["units"].aggregate(pipeline))
+    pipeline = [{'$match': {'tilte': utils.normalize_title(title)}}]
+    return list(ghandler.db['units'].aggregate(pipeline))
 
 
 def get_topic_by_title(title: str) -> dict[str, str]:
-    topic = ghandler.db["topics"].find_one({"title": utils.normalize_title(title)})
+    topic = ghandler.db['topics'].find_one({'title': utils.normalize_title(title)})
     if not topic:
-        return {"ok": False, "msg": "Topic does not exist", "data": None}
+        return {'ok': False, 'msg': 'Topic does not exist', 'data': None}
 
-    topic["units"] = units.get_units(topic.get("_id"))
-    return {"ok": True, "msg": "Success", "data": topic}
+    topic['units'] = units.get_units(topic.get('_id'))
+    return {'ok': True, 'msg': 'Success', 'data': topic}
 
 
 def get_by_titles(
     topic_title: str, unit_title: str, lesson_title: str
 ) -> dict[str, Any]:
-    topic = ghandler.db["topics"].find_one(
-        {"title": utils.normalize_title(topic_title)}
+    topic = ghandler.db['topics'].find_one(
+        {'title': utils.normalize_title(topic_title)}
     )
     if not topic:
-        logging.info("No topic found with the given title.")
-        return {"ok": False, "msg": "No topic found with the given title"}
+        logging.info('No topic found with the given title.')
+        return {'ok': False, 'msg': 'No topic found with the given title'}
 
-    unit = ghandler.db["units"].find_one(
-        {"title": utils.normalize_title(unit_title), "topic_id": topic["_id"]}
+    unit = ghandler.db['units'].find_one(
+        {'title': utils.normalize_title(unit_title), 'topic_id': topic['_id']}
     )
     if not unit:
-        logging.info("No unit found with the given title and topic_id.")
-        return {"ok": False, "msg": "No unit found with the given title and topic_id"}
+        logging.info('No unit found with the given title and topic_id.')
+        return {'ok': False, 'msg': 'No unit found with the given title and topic_id'}
 
-    lesson = ghandler.db["lessons"].find_one(
-        {"title": utils.normalize_title(lesson_title), "unit_id": unit["_id"]}
+    lesson = ghandler.db['lessons'].find_one(
+        {'title': utils.normalize_title(lesson_title), 'unit_id': unit['_id']}
     )
 
     if not lesson:
-        logging.info("No lesson found with the given title and unit_id.")
-        return {"ok": False, "msg": "No lesson found with the given title and unit_id"}
+        logging.info('No lesson found with the given title and unit_id.')
+        return {'ok': False, 'msg': 'No lesson found with the given title and unit_id'}
 
-    topic["units"] = (unit,)
-    unit["lessons"] = (lesson,)
-    lesson["widgets"] = widgets.get_widgets(lesson["_id"])
+    topic['units'] = (unit,)
+    unit['lessons'] = (lesson,)
+    lesson['widgets'] = widgets.get_widgets(lesson['_id'])
     return {
-        "ok": False,
-        "msg": "No lesson found with the given title and unit_id",
-        "data": topic,
+        'ok': False,
+        'msg': 'No lesson found with the given title and unit_id',
+        'data': topic,
     }
 
 
 def get_all() -> tuple[dict, ...]:
-    """
+    '''
     Retrieves title and description for all topics
-    """
+    '''
 
     return tuple(
         document
-        for document in ghandler.db["topics"].find(
-            {}, {"title": 1, "description": 1, "_id": 0}
+        for document in ghandler.db['topics'].find(
+            {}, {'title': 1, 'description': 1, '_id': 0}
         )
     )
